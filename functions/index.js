9182db84a1ff7113c0bb9e6ff8cec890a3853d58
shun1121 <hikitanikun.ware80000@gmail.com>
2022-10-13 18:53:34 +0900

    Revert "🔥"
    This reverts commit 05c460cd2a6047f1f0ce4e5e3922315412684566.

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,32 @@
 const functions = require("firebase-functions");
+
+require('dotenv').config();
+const { createServer } = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { createEventAdapter } = require('@slack/events-api');
 const { WebClient } = require('@slack/web-api');
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET
+const port = process.env.PORT || 3000;
 const slackEvents = createEventAdapter(slackSigningSecret);
 const token = process.env.USER_OAUTH_TOKEN
 const web = new WebClient(token)
-require('dotenv').config();
 
 const app = express();
 
 app.use('/slack/events', slackEvents.requestListener());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
+// // app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/', function(req, res) {
-  res.send("change-status");
+  res.send('Ngrok is working!! Path Hit: ' + req.url);
 });
+
+// const server = createServer(app);
+
+// server.listen(port, () => console.log(`listening on por ${port}!`));
 
 app.post('/slack/command', async (req, res) => {
   if (req.body.command === '/hiru') {

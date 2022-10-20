829bf1c0364da9c936c1c628494e3b6a2d7e136e
shun1121 <hikitanikun.ware80000@gmail.com>
2022-10-20 20:37:04 +0900

    🔥 remove code, need to apply database for tokens

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,12 @@
 require("dotenv").config();
 const functions = require("firebase-functions");
 const express = require("express");
-// const request = require("request");
 const bodyParser = require("body-parser");
 const { createEventAdapter } = require("@slack/events-api");
 const { WebClient } = require("@slack/web-api");
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
 const slackEvents = createEventAdapter(slackSigningSecret);
 const token = process.env.USER_OAUTH_TOKEN;
-// const web = new WebClient(token);
 
 const app = express();
 
@@ -31,7 +29,6 @@ app.get("/", async function (req, res) {
     });
     res.send(`access_token: ${result.authed_user.access_token}`)
     app.post("/slack/command", async (req, res) => {
-      // const web = new WebClient(token);
       const web = new WebClient(result.authed_user.access_token);
       if (req.body.command === "/hiru") {
         const currentTime = Math.floor(Date.now() / 1000);
@@ -58,11 +55,11 @@ app.get("/", async function (req, res) {
             status_expiration: expiration,
           },
         });
-        await web.chat.postMessage({
-          token: result.authed_user.access_token,
-          channel: "#breaktime",
-          text: ":half1:",
-        });
+        // await web.chat.postMessage({
+        //   token: result.authed_user.access_token,
+        //   channel: "#breaktime",
+        //   text: ":half1:",
+        // });
       } else if (req.body.command === "/kouhan") {
         const currentTime = Math.floor(Date.now() / 1000);
         const expiration = currentTime + 1800;
@@ -83,62 +80,9 @@ app.get("/", async function (req, res) {
       }
     });
   } else if (error) {
-    // return '認証がキャンセルされました。';
     res.send("error")
     console.log("エラー");
   }
 });
-
-// app.post("/slack/command", async (req, res) => {
-//   const web = new WebClient(token);
-//   if (req.body.command === "/hiru") {
-//     const currentTime = Math.floor(Date.now() / 1000);
-//     const expiration = currentTime + 3600;
-//     res.send("");
-//     await web.users.profile.set({
-//       profile: {
-//         status_emoji: ":ohiru:",
-//         status_expiration: expiration,
-//       },
-//     });
-//     // await web.chat.postMessage({
-//     //   token: process.env.USER_OAUTH_TOKEN,
-//     //   channel: "#breaktime",
-//     //   text: ":ohiru:",
-//     // });
-//   } else if (req.body.command === "/zenhan") {
-//     const currentTime = Math.floor(Date.now() / 1000);
-//     const expiration = currentTime + 1800;
-//     res.send("");
-//     await web.users.profile.set({
-//       profile: {
-//         status_emoji: ":half1:",
-//         status_expiration: expiration,
-//       },
-//     });
-//     await web.chat.postMessage({
-//       token: process.env.USER_OAUTH_TOKEN,
-//       channel: "#breaktime",
-//       text: ":half1:",
-//     });
-//   } else if (req.body.command === "/kouhan") {
-//     const currentTime = Math.floor(Date.now() / 1000);
-//     const expiration = currentTime + 1800;
-//     res.send("");
-//     await web.users.profile.set({
-//       profile: {
-//         status_emoji: ":half2:",
-//         status_expiration: expiration,
-//       },
-//     });
-//     await web.chat.postMessage({
-//       token: process.env.USER_OAUTH_TOKEN,
-//       channel: "#breaktime",
-//       text: ":half2:",
-//     });
-//   } else {
-//     console.log("error");
-//   }
-// });
 
 exports.app = functions.https.onRequest(app);
